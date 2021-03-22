c69921e761bf5b30e28ee684cb915812edfac889
Pierre <dev@nettools.ovh>
2021-03-22 21:15:54 +0100

    test

--- tests/TwigTest.php
+++ tests/TwigTest.php
@@ -6,35 +6,22 @@ namespace Nettools\Mailing\MailSenderHelpers\Tests;
 
 use \Nettools\Mailing\Mailer;
 use \Nettools\Mailing\MailSenderHelpers\Twig;
-use \org\bovigo\vfs\vfsStream;
-use \org\bovigo\vfs\vfsStreamDirectory;
 
 
 
 class TwigTest extends \PHPUnit\Framework\TestCase
 {
-	protected $_vfs = NULL;
-	protected $root;
-	
-
-	public function setUp() :void
-	{
-		$this->_vfs = vfsStream::setup('root');
-		$this->root = vfsStream::url("root/");
-	}
-	
-	
-		
 	public function testMSH()
 	{
-		$ml = new Mailer(new \Nettools\Mailing\MailSenders\Virtual());
+    	$ms = new \Nettools\Mailing\MailSenders\Virtual();
+		$ml = new Mailer($ms);
 		$msh = new Twig($ml, 'msh content #{{ name }}#', 'text/plain', 'unit-test@php.com', 'test subject');
 
 		$mcontent = $msh->render(['name' => 'me !']);
 		$msh->send($mcontent, 'recipient@here.com');
 		
-		$this->assertEquals(1, count($ml->getSent()));
-		$this->assertEquals(true, is_int(strpos($ml->getSent()[0], 'msh content #me !#')));
+		$this->assertEquals(1, count($ms->getSent()));
+		$this->assertEquals(true, is_int(strpos($ms->getSent()[0], 'msh content #me !#')));
 	}
 }
 
